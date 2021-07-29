758e2ec8f22746abb797d145d8d35be8222632e2
adastraperasper <77408221+adastraperasper@users.noreply.github.com>
2021-07-29 09:44:29 -0400

    Add files via upload

---
+++ labels.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+
+#label = an area widget that holds text and/or an image within a
+# window
+
+window = Tk()
+
+photo = PhotoImage(file = "C:\\Users\\Home\\Desktop\\map1.png")
+
+label = Label(window, text="Hello World", font= ("Arial",40,"bold"),
+              fg="#00FF00",
+               bg="black",
+               relief= RAISED,
+               padx=20,
+               pady=20,
+               image= photo,
+               compound ='bottom')
+label.pack()
+#label.place(x=0,y =0)
+
+
+window.mainloop()
+
